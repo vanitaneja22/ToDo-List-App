6d9c681765ca1ce7050e31825aaa692f76225bd0
vanitaneja22 <vani.taneja123@gmail.com>
2019-01-17 13:19:39 -0800

    todocommit

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grab a new todo
+		var todotext = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todotext + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
